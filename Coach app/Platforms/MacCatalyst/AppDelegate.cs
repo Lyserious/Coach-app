94bd562ed173493592b31a037a1493ddf7f45f2f
Lyserïous <Loic.serruys@student.uclouvain.be>
2026-02-03 17:00:14 +0100

    Ajoutez des fichiers projet.

---
+++ Coach app/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Coach_app
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
